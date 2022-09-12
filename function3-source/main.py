ae77ef077c2a451c8f22135e763d9ca400e8241c
Lukasz Grabarek <grabarek@prodigy.net>
2022-09-12 13:37:56 -0800

    Add files via upload
    Scheduling setup on GCP

---
+++ function3-source/main.py
@@ -0,0 +1,41 @@
+import json
+from google.cloud import aiplatform
+
+
+project_id = "curious-skyline-360213"
+dataset_id = "CX"
+pipeline_root_path = "gs://curious-skyline/Projects/CX/CXpipeline"
+
+def process_request(request):
+   """Processes the incoming HTTP request.
+
+   Args:
+     request (flask.Request): HTTP request object.
+
+   Returns:
+     The response text or any set of values that can be turned into a Response
+     object using `make_response
+     <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+   """
+
+   # decode http request payload and translate into JSON object
+   request_str = request.data.decode('utf-8')
+   request_json = json.loads(request_str)
+
+   pipeline_spec_uri = request_json['pipeline_spec_uri']
+   parameter_values = request_json['parameter_values']
+
+   aiplatform.init(
+       project=project_id,
+       staging_bucket=pipeline_root_path
+       )
+
+   job = aiplatform.PipelineJob(
+    display_name='cx-pipeline-process-data',
+    template_path=pipeline_spec_uri,
+    pipeline_root=pipeline_root_path,
+    enable_caching=False,
+    parameter_values=parameter_values
+       )
+
+   job.submit()
